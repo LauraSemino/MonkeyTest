013a5d9fdfe3a78cab4dbae0a995eac8081e1e29
LauraSemino <150286546+LauraSemino@users.noreply.github.com>
2026-03-02 14:09:11 -0500

    Initial Commit

---
+++ Assets/Scripts/Grabbing.cs
@@ -0,0 +1,32 @@
+using StarterAssets;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class Grabbing : MonoBehaviour
+{
+    private StarterAssetsInputs _input;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _input = GetComponent<StarterAssetsInputs>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_input.lgrab == true)
+        {
+            //do thing
+        }
+        else
+        {
+            _input.lgrab = false;
+        }
+    }
+   // private void OnTriggerStay(Collider obj)
+  //  {
+        //check mass
+        
+
+  //  }
+}
