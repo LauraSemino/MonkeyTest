86080e8e0dc1a66ac031b47bb64e149f1aede1d5
LauraSemino <150286546+LauraSemino@users.noreply.github.com>
2026-03-02 17:46:42 -0500

    Pull added

--- Assets/Scripts/ArmControl.cs
+++ Assets/Scripts/ArmControl.cs
@@ -6,8 +6,8 @@ using UnityEngine.Windows;
 public class ArmControl : MonoBehaviour
 {
     public GameObject hand;
-    
-
+    public CharacterController controller;
+    public float PullTimeout = 0.4f;
 
     private StarterAssetsInputs _input;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,17 +19,25 @@ public class ArmControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        hand.transform.position += (new Vector3(_input.moveArm.x, _input.moveArm.y, 0)) * Time.deltaTime;    
-       
+        hand.transform.position += (new Vector3(_input.moveArm.x, _input.moveArm.y, 0)) * Time.deltaTime;
+        if (PullTimeout > 0f && _input.lgrab == true)
+        {
+            controller.Move((hand.transform.position - transform.position) * Time.deltaTime * 5);
+            PullTimeout -= Time.deltaTime;
+        }
+
+        if (PullTimeout <= 0)
+        {
+            _input.lgrab = false;
+            PullTimeout = 0.4f;
+        }
+
     }
     private void LateUpdate()
     {
        
     }
-    public void OnLGrab(InputValue value)
-    {
-        
-    }
+  
    
 
 
